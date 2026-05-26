32297326c292b94279f3e6b359941f239640fd0e
dwrqedadaweq-ops <dwrqedadaweq@gmail.com>
2026-05-26 23:29:07 +0000

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,17 +1,25 @@
-let gameStatus = {
-    Terrain: false,
-    Lighting: false,
-    Storage: false,
-    Players: false
-};
+let activeServers = {};
 
 export default function handler(req, res) {
     if (req.method === 'POST') {
-        // Roblox sends data here
-        gameStatus = req.body;
-        return res.status(200).json({ message: "Updated" });
-    } else {
-        // Website reads data from here
-        return res.status(200).json(gameStatus);
+        const payload = req.body;
+        const serverId = payload.ServerId;
+        
+        activeServers[serverId] = {
+            status: payload.Status,
+            lastUpdated: Date.now()
+        };
+        
+        return res.status(200).json({ success: true, url: `/?id=${serverId}` });
+    } 
+
+    if (req.method === 'GET') {
+        const { id } = req.query;
+        
+        if (id) {
+            return res.status(200).json(activeServers[id] || { error: "server off or not found" });
+        }
+        
+        return res.status(200).json(activeServers);
     }
 }
